8b56806116b1fb59ec27cc5bcb6c86f21d9182a2
Layza C <layzacs@localhost.localdomain>
2019-02-22 14:29:31 -0300

    Add content about variables.

---
+++ aula4/hello.c
@@ -0,0 +1,14 @@
+// Hello World
+
+// gcc teste.c -o teste - create the teste file ready to execute
+// ./teste - executing the file created
+
+#include <stdio.h>
+
+
+main()
+{
+  printf("Hello World 1\n");
+  printf("Hello World 2\n");
+  printf("Hello World 3\n\n");
+}
---
+++ aula4/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//If the variable is declared outside of a fuction, it will function globally. This is not a good idea. Keep them inside.
+
+main()
+{
+  /* aqui começam as declarações de variáveis */
+  int x; /* declaração de uma variável inteira */
+  float y;  /* declaração de uma variável real */
+
+  /* aqui começam as instruções do programa principal */
+  x=5;   /* atribuindo o valor 5 (constante) a variável x */
+  y=6.5;
+}
