ffe9ecc7cb2ecf6f93f7ba7263a0b7d8261e2fca
Layza C <layzacs@localhost.localdomain>
2019-02-22 15:20:38 -0300

    Add ex1.

---
+++ aula4/ex1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main()
+{
+  // Declaring variables
+  int r1,r2;
+  // Input values for R1 e R2
+  printf("Entre com R1\n");
+  scanf("%f", &R1);
+  printf("Entre com R2\n");
+  scanf("%f", &R2);
+
+// Calculating serie
+
+  
+
+
+}
