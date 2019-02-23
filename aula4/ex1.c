d4edb8716d0216308221957537104f13f9c3f101
Layza C <layzacs@localhost.localdomain>
2019-02-23 15:30:28 -0300

    Add ex.1.

--- aula4/ex1.c
+++ aula4/ex1.c
@@ -3,16 +3,23 @@
 main()
 {
   // Declaring variables
-  int r1,r2;
+  int R1,R2;
+  float serie,paralelo;
   // Input values for R1 e R2
   printf("Entre com R1\n");
   scanf("%f", &R1);
   printf("Entre com R2\n");
   scanf("%f", &R2);
 
-// Calculating serie
+  // Calculating serie
 
-  
+  serie=R1+R2
 
+  // Calculating paralel
+
+  paralelo=(R1*R2)/(R1+R2)
+
+  printf ("O valor da série é %d\n",serie);
+  printf ("O valor de x é %d\n",paralelo);
 
 }
