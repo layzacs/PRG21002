5719662d54c70d859712401eb9c9cc74055c9d01
layzacs <layza.cristini@gmail.com>
2019-02-27 13:56:05 -0300

    Update decision.c

--- aula5/decision.c
+++ aula5/decision.c
@@ -3,8 +3,7 @@
 main()
 {
  
-  int x; /* declaração de uma variável inteira */
-  int y;  /* declaração de uma variável inteira */
+  int x,y,z; /* declaração de uma variável inteira */
  
   printf ("Entre com o valor de x ");
   scanf("%d",&x);
@@ -12,13 +11,14 @@ main()
   printf ("Entre com o valor de y ");
   scanf("%d",&y);
  
-  if (y>x)
-     printf("MSG1:y é maior que x\n");
- 
- 
-  if (y>x)
-     printf("MSG2:y é maior que x\n");
-  else
-     printf("MSG3:y é igual ou menor que x\n");
+  if (y>x) {
+     printf("MSG1: y é maior que x\n");
+     z = y-x;
+     printf("MSG2: Neste caso z =  %d\n", z);
+  } else {
+     printf("MSG3: y é igual ou menor que x\n");
+     z = x-y;
+     printf("MSG4: Neste caso z =  %d\n", z);
+  }
  
 }
