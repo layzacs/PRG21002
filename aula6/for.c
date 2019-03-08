e15a8c3dea18e6616965d41355e82c68b3284941
layza cristini <layza.cristini@gmail.com>
2019-03-08 13:53:44 -0300

    Create for.c

---
+++ aula6/for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+main()
+{
+  int i;
+ 
+// for(expressão_inicial;expressão_de_controle; expressão_de_final_de _bloco) {
+//    lista_de_instruções
+//}
+ 
+  for (i=0;i<10;i=i+1) {
+      printf("i =%d\n",i);
+  }
+}
