b8735111b07e9ca869598c1c4eab914b058519e6
layza cristini <layza.cristini@gmail.com>
2019-03-08 13:51:11 -0300

    Update while.c

--- aula6/while.c
+++ aula6/while.c
@@ -11,3 +11,16 @@ main() {
 
   printf("NO FINAL a expressão  contador < 5 é %d\n", contador<5);
 }
+
+// Another possible format: while in the end of the instruction
+//#include <stdio.h>
+//main() 
+//{
+//  int contador;
+// 
+//  contador=0;
+//  do {  
+//     printf("valor do contador =  %d\n", contador);
+//     contador=contador+1;
+//  } while (contador<5);
+//}
