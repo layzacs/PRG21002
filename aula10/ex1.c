6acfd6e7b0510667ecd3c37870ab11b28169b7b1
layza cristini <layza.cristini@gmail.com>
2019-03-29 15:19:57 -0300

    Create ex1.c

---
+++ aula10/ex1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int i,valor;
+  char n[5];
+
+  printf("Digite 5 números:\n");
+  scanf("%s", n);
+  printf("A string é %s\n", n);
+  printf ("%c\n", n[1]);
+  printf ("%c\n", n[4]);
+  for (i=4; i<0; i--) {
+    printf("Valor do elemento %d da string: %c\n", i, n[i]);
+    printf("O loop está em andamento.\n");
+  }
+
+}
