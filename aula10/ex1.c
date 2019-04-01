97565945994593b0706f2cb8b1592c78f4929910
Layza C <layzacs@localhost.localdomain>
2019-03-31 22:22:18 -0300

    Fix ex1.c

--- aula10/ex1.c
+++ aula10/ex1.c
@@ -7,12 +7,8 @@ int main(void) {
 
   printf("Digite 5 números:\n");
   scanf("%s", n);
-  printf("A string é %s\n", n);
-  printf ("%c\n", n[1]);
-  printf ("%c\n", n[4]);
-  for (i=4; i>0; i--) {
-    printf("Valor do elemento %d da string: %c\n", i, n[i]);
-    printf("O loop está em andamento.\n");
-  }
 
+  for (i=5; i>=0; i--) {
+    printf("%c\n", n[i]);
+  }
 }
