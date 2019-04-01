772604a123caccba0e8163b715d7fccd59f9af60
layzacs <layzacs@localhost.localdomain>
2019-04-01 12:14:35 -0300

    Fix ex6.c

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ Conteúdo e exercícios da disciplina de PRG29002 (Programação 1) do curso de 
 ## Páginas contendo as aulas:
 https://wiki.sj.ifsc.edu.br/wiki/index.php/PRG29002_-_Programa%C3%A7%C3%A3o_I_-_Eng.Telecom_2019-1
 
+## Troubleshooting
+
+O comando abaixo pode ajudar com algumas dicas extras, na hora de compilar o código:
+
+```
+gcc main.c -Wall -Wextra
+```
+
+### Repl.it
--- aula10/ex6.c
+++ aula10/ex6.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main () {
-  int i, ntotal=0;
+  int i;
   char stp[40], sts[40], troca=0;
 
   printf("Entre com a frase número 1:\n");
@@ -14,12 +14,12 @@ int main () {
   printf("As frases são: %s e %s\n", stp, sts);
 
   for (i=0;i<40;i++) {
-    if (stp[i]!=sts[i] & stp[i]!=0 & sts[i]!=0) {
+    if ((stp[i]!=sts[i]) & (stp[i]!=0) & (sts[i]!=0)) {
       troca = stp[i];
       stp[i] = sts[i];
       sts[i] = troca;
     }
-    else if (stp[i]==0 & sts[i]==0)
+    else if ((stp[i]==0) & (sts[i]==0))
       break;
   }
   printf("O resultantes: %s e %s\n", stp, sts);
