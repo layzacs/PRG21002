d8fc9a2181823caa8d7795849f86eaa38f7ef08b
layza cristini <layza.cristini@gmail.com>
2019-04-03 14:22:19 -0300

    Create parameters-functions.c

---
+++ aula11/parameters-functions.c
@@ -0,0 +1,13 @@
+main()
+{
+  float media, x,y,z;
+ 
+  x = 5.7;
+  y = 9.8;
+ 
+  /* exemplo de chamada 1 */  
+  media = media_nums(4.8,x,y*3);
+ 
+  /* exemplo de chamada 2 */ 
+  media = media_nums (x+y,y*y+5,(x+y)/2);
+}
