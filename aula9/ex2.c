174524a03b49a685fa5b0af53f91f5ac3cf1f2af
layza cristini <layza.cristini@gmail.com>
2019-04-12 14:38:21 -0300

    Update ex2.c

--- aula9/ex2.c
+++ aula9/ex2.c
@@ -4,10 +4,11 @@ int main(void) {
 
   int vet[10],total, soma=0, i;
   
-  for (i=0;i<10;i++) {
+  while (i<10) {
   printf("Insira o valor de vet[%d]:\n", i);
   scanf("%d", &vet[i]);
   soma=soma+vet[i];
+  i++;
   }
   printf("A média é: %d\n", (soma/10));
 }
