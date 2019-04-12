d6c817704017b5668ebb6d20bd6ed76ba1145a1e
layza cristini <layza.cristini@gmail.com>
2019-04-12 14:36:10 -0300

    Create ex4.c

---
+++ aula9/ex4.c
@@ -0,0 +1,20 @@
+// 4. Refazer os exercícios anteriores usando o comando 'for'. 
+#include <stdio.h>
+
+int main(void) {
+
+  int vet[10],total, soma=0, i;
+  
+  for (i=0;i<10;i++) {
+  printf("Insira o valor de vet[%d]:\n", i);
+  scanf("%d", &vet[i]);
+  soma=soma+vet[i];
+  }
+  for (i=0;i<10;i++) {
+    if ((soma/10)<vet[i]) {
+      total++;
+    }
+  }
+  printf("A quantidade de numeros maiores que a média é: %d\n", total);
+
+}
