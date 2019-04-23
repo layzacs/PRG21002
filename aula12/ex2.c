afda88e65100f66b8de9c2435eb16bef75e625ce
layza cristini <layza.cristini@gmail.com>
2019-04-23 14:33:28 -0300

    Update ex2.c

--- aula12/ex2.c
+++ aula12/ex2.c
@@ -1,1 +1,25 @@
 // 2. Implementar uma programa para calcular o determinante de uma matriz 3x3 (de reais) a ser fornecida pelo teclado.
+
+#include <stdio.h>
+
+int det_mat(int mc [3][3]) {
+  int i,j;
+
+  for(i=0;i<TAM_LIN;i++){
+     for(j=0;j<TAM_COL;j++) {
+        
+     }
+  }
+}
+
+void main() {
+  int mat[3][3], i=0, j=0;
+
+  for(i=0;i<3;i++) {
+    for(j=0;j<3;j++) {
+      printf("Entre com o valor de mat[%d][%d]\n", i, j);
+      scanf(%d, &mat[i][j])
+    }
+  }
+  
+}
