8537b19f9187d6699d67f58dcfe327cfbaf7963b
layza cristini <layza.cristini@gmail.com>
2019-04-23 19:17:51 -0300

    Update ex3.c

--- aula12/ex3.c
+++ aula12/ex3.c
@@ -1,1 +1,2 @@
-// Implementar um programa para ler duas matrizes (matA e matB) e multiplicá-las, colocando o resultado em uma matriz matC. Assumir dimensões de matA e matB de 2x3 e 3x4 respectivamente.
+// 3. Implementar um programa para ler duas matrizes (matA e matB) e multiplicá-las, colocando o resultado em uma matriz matC. Assumir dimensões de matA e matB de 2x3 e 3x4 respectivamente.
+
