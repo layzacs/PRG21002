e58dcc84915eb25e762a37e81fe1553f2680abd4
layza cristini <layza.cristini@gmail.com>
2019-05-22 13:51:18 -0300

    Create estructure-inside-structure.c

---
+++ aula13-estruturas/estructure-inside-structure.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+ 
+struct TEndereco{
+     char rua[50];
+     char numero[10];
+};
+ 
+struct TCidadao{
+  char nome[50];
+  char cpf[20];
+  struct TEndereco endereco;
+  int num_filhos;
+};
+ 
+void main()
+{
+  struct TCidadao Cidadao;
+ 
+  printf("Entre com o nome\n");
+  scanf ("%s",Cidadao.nome);
+ 
+  printf("Entre com o cpf\n");
+  scanf ("%s",Cidadao.cpf);
+ 
+  printf("Entre a rua\n");
+  scanf ("%s",Cidadao.endereco.rua);
+ 
+  printf("Entre a numero\n");   
+  scanf ("%s",Cidadao.endereco.numero);
+ 
+  printf("Entre com o número de filhos\n");
+  scanf ("%d",&Cidadao.num_filhos);
+ 
+}
