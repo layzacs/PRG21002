de77032132c88f06dce66091ac4d9bb129ed9e90
layza cristini <layza.cristini@gmail.com>
2019-05-31 14:15:41 -0300

    Create ex2-swap.c

---
+++ aula14-ponteiros/ex2-swap.c
@@ -0,0 +1,16 @@
+//Implemente um programa que realize o swap de suas variáveis inteiras passadas como parâmetro. Use ponteiros.
+#include <stdio.h>
+
+int main(void) {
+  int alfa, beta, aux;
+
+  a = &alfa;
+  b = &beta;
+
+  aux = *a;
+  *a = *b;
+  *b = aux;
+
+  return 0;
+
+}
