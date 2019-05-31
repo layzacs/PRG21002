7392ff4c717929cf0a38d5ee73ae17512a940471
layza cristini <layza.cristini@gmail.com>
2019-05-31 15:21:41 -0300

    Update ex3.c

--- aula13-estruturas/ex3.c
+++ aula13-estruturas/ex3.c
@@ -12,28 +12,29 @@ struct num_complex {
 };
 
 
-float soma_mat_complex(struct num_complex x, struct num_complex y) {
+struct num_complex soma_mat_complex(struct num_complex x, struct num_complex y) {
+
+  x.real = x.real + y.real;
+  x.complex = x.complex + y.complex;
 
 }
 
 
 int main (void) {
 
-  struct num_complex n1={2,5};
-
-
-
   struct num_complex mat1[NUM_LIN][NUM_COL]={
                               {{2.5,2.7},{1.5,2.3}},
                               {{1.9,2.6},{3.5,7.3}}
-                            };
+                            }, 
                       mat2[NUM_LIN][NUM_COL]={
-                              {{.x=4.5,.y=2.8,}, {.x=1.6,.y=8.3,}},
-                              {{.x=0.9,.y=2.9,}, {.x=7.5,.y=4.3,}}
+                              {{4.5,2.8},{1.6,8.3}},
+                              {{0.9,2.9},{7.5,4.3}}
                             };
 
   //printf("mat[1][1].real = %f\n", mat1[0][1].real);
 
-  struct soma_mat_complex(mat1, mat2);
+  soma_mat_complex(mat1, mat2);
+
+  printf("A soma deu: %f + (%fi)\n", mat1.real, mat1.complex);
 
 }
